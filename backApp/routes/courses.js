5c4c013033f4e7eed86765cf5f33cb255a0900db
Mina Ehab Khalil <minaehab161.me@gmail.com>
2022-10-20 14:53:47 +0200

    Requirement 20

--- backApp/routes/courses.js
+++ backApp/routes/courses.js
@@ -1,37 +1,28 @@
 var express = require('express');
 var router = express.Router();
-const { DB_URL } = require('./consts.json')
-const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
-
-//MongoDB Client
-const client = new MongoClient(DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-//Connecting to database using mongoose
-mongoose.connect(DB_URL,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
-})
-
-//Schema Definition
-var courseSchema = new mongoose.Schema({
-    title: String,
-    subtitle: String,
-    subject: String,
-    summary: String,
-    instructorId: Number,
-})
-
-var Course = mongoose.model("Course",courseSchema);
+const Course = require('../models/Course');
 
 /* GET Courses listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/search/instructor', async function(req, res) {
+  var data = req.query
+  var results = await searchCourse(data)
+  res.writeHead(200,{"Content-Type": "application/json"})
+  res.write(JSON.stringify(results))
+  res.end()
+});
+
 /* Functions */
+
+async function searchCourse(data){
+  var query = ".*"+data.query+".*"
+  const mongoQuery = { $and: [{instructorId: data.instructorId},{$or: [{subject: {$regex: new RegExp(query, 'i')}}, {title: {$regex: new RegExp(query, 'i')}}]}]}
+  var results = await Course.find(mongoQuery)
+  return results
+}
 
 module.exports = router;
