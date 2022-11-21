d2fb802dd9f156c28c5684375b9f3bf5be708387
johnnabil <john.nabil10@yahoo.com>
2022-11-21 17:02:28 +0200

    Updated files based on Comments

--- backApp/models/Exercise.js
+++ backApp/models/Exercise.js
@@ -4,7 +4,6 @@ var ExerciseSchema = new mongoose.Schema({
     title: {type: String, required: true},
     question: {type: Array, required: true},
     answer: {type: Array, required: true},
-    courseId: {type: mongoose.Types.ObjectId, ref:"Course" ,required: true}
 }, {timestamps: true});
 
 var Exercise = mongoose.model("Exercise",ExerciseSchema);
--- backApp/models/Video.js
+++ backApp/models/Video.js
@@ -4,7 +4,6 @@ var VideoSchema = new mongoose.Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     url: {type: String, required: true},
-    courseId: {type: mongoose.Types.ObjectId, ref:"Course" ,required: true}
 }, {timestamp: true});
 
 var Video = mongoose.model("Video",VideoSchema);
